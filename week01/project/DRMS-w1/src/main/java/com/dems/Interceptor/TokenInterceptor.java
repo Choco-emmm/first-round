3b1d9bda17200f7bd34b2f87317deede73076197
Choco <renpei233@gmail.com>
2026-03-21 23:30:34 +0800

    采用ThreadLocal来储存登录的信息，让Service层可以直接拿到信息。顺便改了一些bug

--- week01/project/DRMS-w1/src/main/java/com/dems/Interceptor/TokenInterceptor.java
+++ week01/project/DRMS-w1/src/main/java/com/dems/Interceptor/TokenInterceptor.java
@@ -3,11 +3,14 @@ package com.dems.Interceptor;
 
 import com.dems.enums.UserRole;
 import com.dems.utils.jwtUtil;
+import com.dems.utils.UserContext;
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class TokenInterceptor implements HandlerInterceptor {
@@ -26,31 +29,43 @@ public class TokenInterceptor implements HandlerInterceptor {
         //4.判断token是否存在，若不存在说明用户没有登录，返回401
         if (token == null) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            System.out.println("用户未登录");
             return false;
         }
 
         //5.如果令牌存在，校验令牌，如果校验失败，也返回401
         //只要无法解析就是非法令牌，直接处理错误就行
         try {
-            jwtUtil.parseJwt(token);
+            Claims claims = jwtUtil.parseJwt(token);
+            //从令牌中获取claims并存入threadlocal以便之后的方法取用
+            UserContext.setData(claims);
         } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            System.out.println("非法令牌");
             return false;
         }
         //6.获取访问路径,如果是学生专用就看角色是不是学生，是就放行，不是就不放行。管理员的亦然
         String path = request.getRequestURI();
-        Integer role = jwtUtil.getUserRoleFromJwt(token);
+        Integer role = UserContext.getUserRole();
         if(path.contains("/student")){
             if(!role.equals(UserRole.STUDENT.getCode() )){
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                System.out.println("用户不是学生");
                 return false;
             }
         }else if(path.contains("/admin")){
             if(!role.equals(UserRole.ADMIN.getCode())){
                 response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                System.out.println("用户不是管理员");
                 return false;
             }
         }
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        //清除数据
+        UserContext.remove();
     }
 }
